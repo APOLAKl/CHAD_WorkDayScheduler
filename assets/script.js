56f31997fed194e4ae943a49e07158e1cc072d27
APOLAKl <122183428+APOLAKl@users.noreply.github.com>
2023-02-16 22:33:49 -0400

    Fourth commit - added descriptive comments

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// call to jQuery to ensure that the code isn't run until the browser
+// has finished rendering all the elements in the html.
 $(document).ready(function() {
-  // TODO: Add code to display the current date in the header of the page.
+  // handle displaying the time
   var today = dayjs();
-  $('#today').text(today.format('MMM D, YYYY hA'));
+  $('#today').text(today.format('MMM D, YYYY [at] hA'));
 
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -15,9 +14,10 @@ $(document).ready(function() {
   // useful when saving the description in local storage?
   
   // saving
-
   var saveButtons = $(".saveBtn");
 
+  // on click event listener in the containing time-block
+  // saving key and value
   saveButtons.on("click", function() {
     var key = $(this).parent().attr("id");
     var value = $(this).siblings("textarea").val()
@@ -28,14 +28,10 @@ $(document).ready(function() {
     localStorage.setItem(key, value)
   })
 
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
    // loading
   var textAreas = $(".description");
 
+  // load saved key and value
   textAreas.each(function() {
     var key = $(this).parent().attr("id");
     var value = localStorage.getItem(key)
@@ -43,14 +39,7 @@ $(document).ready(function() {
     $(this).val(value)
   })
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
   // color coding 
-  
   var txtAreas = $(".description");
 
   txtAreas.each(function () {
@@ -66,6 +55,7 @@ $(document).ready(function() {
     console.log(currentHour)
     console.log(textHour)
 
+    // Apply the past, present, or future class to value block
     if(currentHour == textHour) {
       $(this).addClass("present")
     }
